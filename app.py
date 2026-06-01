8ec26dc2eac53479f6814cba1927cf9310cce53c
NosovaYevheniia <zhenianosova2006@gmail.com>
2026-06-01 16:59:50 +0200

    2026-06-01 YN

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
+
+@app.route("/<book>")
+def show_book(book):
+    return render_template(f"{book}.html")
+
+if __name__ == "__main__":
+    app.run()
